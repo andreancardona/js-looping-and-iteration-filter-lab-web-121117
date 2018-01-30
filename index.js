b7c8aedefa767b80c9e7045a7786cc4e57d336ae
github username <github email address>
2018-01-30 17:16:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file
+
+
+function findMatching(array, name){
+  let array_of_names =[];
+   for (const driver of array){
+     if (driver.toLowerCase() === name.toLowerCase()) {
+       array_of_names.push(driver);
+     }
+   }
+   return array_of_names;
+}
+
+
+function fuzzyMatch(array, letters){
+   let array_of_names =[];
+  for (const element of array){
+      if (element.slice(0,letters.length) === letters) {
+        array_of_names.push(element);
+      }
+    }
+   return array_of_names;
+  }
+
+
+
+  function matchName(array, string){
+     let array_of_names =[];
+    for (const object of array){
+        if (object.name === string){
+          array_of_names.push(object);
+        }
+      }
+     return array_of_names;
+  }
+
+
+
+//MatchName - This function takes an array of
+//drivers and a string as arguments.
+//In this function, each element of the drivers
+//array is a JavaScript object that has a property
+//of name. The function should return each element
+//whose name property matches the provided string argument.
